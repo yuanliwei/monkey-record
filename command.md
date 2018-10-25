d404f1d797f400a200685cfbf11b2ec6a930eca5
yuanliwei <891789592@qq.com>
2018-10-26 00:44:06 +0800

    add test2 test3

---
+++ command.md
@@ -0,0 +1,14 @@
+adb shell monkey --port 1080
+adb forward tcp:1080 tcp:1080
+telnet 127.0.0.1 1080
+
+sleep 300
+quit
+done
+type string
+press keycode
+tap x y
+wake
+flip [open|close]
+trackball dx dy
+touch [down|up|move] keycode
--- test1.js
+++ test1.js
@@ -17,14 +17,22 @@ touch [down|up] keycode
 */
 
 const net = require('net');
+const { execSync } = require('child_process');
 
 let HOST = '127.0.0.1'
 let PORT = 1080
 
+execSync('adb shell monkey --port 1080\n')
+execSync('adb forward tcp:1080 tcp:1080\n')
 let client = new net.Socket()
 client.connect(PORT, HOST, ()=>{
   console.log('connected to : ' + HOST+' '+PORT);
-  down(455, 234)
+  for (var i = 0; i < 30; i++) {
+    down(155, 234)
+    up(555, 234)
+    sleep(500)
+  }
+  adbexec('quit')
 })
 client.on('data', (data)=>{
   console.log('DATA:'+data);
@@ -44,4 +52,10 @@ function down(x, y) {
 function up(x, y) {
   client.write(`touch move ${parseInt(x)} ${parseInt(y)}\n`)
   client.write(`touch up ${parseInt(x)} ${parseInt(y)}\n`)
+}
+function sleep(time) {
+  client.write(`sleep ${parseInt(time)}\n`)
+}
+function adbexec(command) {
+  client.write(`${command}\n`)
 }
